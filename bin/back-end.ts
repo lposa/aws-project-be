be8fb01bd07bb0867a1b87337e1d260921eb191a
Leonard <leonard_posa@epam.com>
2025-05-27 07:14:40 +0200

    added authorization stack, updated import service to use basic authorization

--- bin/back-end.ts
+++ bin/back-end.ts
@@ -3,6 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ProductServiceStack } from '../lib/products-service/product-service-stack';
 import { DatabaseStack } from '../lib/database/database-stack';
 import { ImportServiceStack } from '../lib/import-service-stack/import-service-stack';
+import { AuthorizationServiceStack } from '../lib/authorization-service/authorization-service-stack';
 
 const app = new cdk.App();
 const productServiceStack = new ProductServiceStack(app, 'ProductsService');
@@ -10,3 +11,4 @@ new DatabaseStack(app, 'DatabaseStack', {});
 new ImportServiceStack(app, 'ImportServiceStack', {
   catalogItemsQueue: productServiceStack.catalogItemsQueue,
 });
+new AuthorizationServiceStack(app, 'AuthorizationServiceStack', {});
